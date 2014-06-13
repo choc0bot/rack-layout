330e0474569d1ab115a58d8160c39af6a396004c
choc0bot <choc0bot@gmail.com>
2014-06-13 18:33:51 +1000

    fixed slider
    dialogs are broken

--- racktest.php
+++ racktest.php
@@ -20,7 +20,8 @@
 $(function() {
 
 $( document ).ready(function() {
-  var value = $('.racksize :selected').text();
+  var value = $( ".racksize" ).val(); 
+  //var value = $('.racksize :selected').text();
   //alert(value);
   $('.number').empty();
   $('.gamma li').remove();
@@ -31,8 +32,9 @@ $( document ).ready(function() {
 
 });
 
-$( ".racksize" ).change(function() {
-  var value = $('.racksize :selected').text();
+$( ".racksize" ).on('slidestop', function(){
+  var value = $( ".racksize" ).val(); 
+  //var value = $('.racksize :selected').text();
   //alert(value);
   $('.number').empty();
   //$('.gamma li').remove();
@@ -81,20 +83,23 @@ $('.gamma').sortable({
         ui.sender.data('copied', true);
   }
 
+//end
 });
 
 
 
 $(".trim").click( function()
   {
-  var rsize = parseInt($('.racksize :selected').text());
+  var rsize = parseInt($( ".racksize" ).val());  
+  //var rsize = parseInt($('.racksize :selected').text());
   var value = rsize + $('.gamma .oneru').length + 2 * $('.gamma .tworu').length + 3 * $('.gamma .threeru').length; 
   //$('.count').append(value);
   while (value > (2 * rsize)){
     var lilength = $('.gamma li').length - 1;
     //$('.count').append('<li>'+ lilength +'</li>');
     $('.gamma li').slice(lilength).remove();
-    var rsize = parseInt($('.racksize :selected').text());
+    var rsize = parseInt($( ".racksize" ).val()); 
+    //var rsize = parseInt($('.racksize :selected').text());
     var value = rsize + $('.gamma .oneru').length + 2 * $('.gamma .tworu').length + 3 * $('.gamma .threeru').length; 
   }
   });
@@ -149,8 +154,15 @@ $(function() {
 </head>	
 
 <div class="dialog-confirm" title="Delete all?"></div>
+    <div ></div>
 
 
+    <div data-role="header" >
+      <a data-iconpos="notext" data-role="button" data-icon="home" title="Home">Home</a>
+      <h1>Rack Layout</h1>
+      <a data-iconpos="notext" href="#panel" data-role="button" data-icon="flat-menu"></a>
+    </div>
+<div data-role="content" role="main">
 <div class="alpha rackdiv">
   <div data-role="collapsible-set" data-theme="b" data-content-theme="b">
     <div data-role="collapsible" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross" data-collapsed="false">
@@ -159,30 +171,32 @@ $(function() {
         <li class="oneru switch">Cisco 3750 24 port</li>
         <li class="oneru switch">Cisco 3750 48 port</li>
         <li class="oneru switch">Cisco 3750X 48 port</li>
+        <li class="oneru switch">Juniper SRX240</li>
         <li class="oneru switch">Cisco 1841</li>
         <li class="oneru switch">Cisco 2960</li>
-        <li class="tworu switch">Cisco 2950</li>
         <li class="oneru switch">Cisco ASR 1000</li>
         <li class="oneru switch">Cisco Nexus 5000</li>
+        <li class="tworu switch">Cisco 1941</li>
+        <li class="tworu switch">Cisco 2950</li>
         <li class="threeru switch">Cisco 3925</li>
         <li class="threeru switch">Cisco 3925 48 port</li>
-        <li class="oneru switch">Juniper SRX240</li>
       </div>
     </div>
-    <div data-role="collapsible" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
+    <div data-role="collapsible" data-theme="f" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
     <h5> Servers</h5>
       <div>
-        <li class="tworu server">DL380 G7</li>
+        <li class="tworu server">HP DL380 G7</li>
       </div>
     </div>
-    <div data-role="collapsible" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
+    <div data-role="collapsible" data-theme="d" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
     <h5> UPS</h5>
       <div>
-        <li class="tworu ups">2KVA UPS</li>
-        <li class="threeru ups">3KVA UPS</li>
+        <li class="tworu ups">APC 1KVA UPS</li>
+        <li class="tworu ups">APC 2KVA UPS</li>
+        <li class="threeru ups">APC 3KVA UPS</li>
       </div>
     </div>
-    <div data-role="collapsible" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
+    <div data-role="collapsible" data-theme="c" data-collapsed-icon="flat-plus" data-expanded-icon="flat-cross">
     <h5> Misc</h5>
       <div>
         <li class="oneru dpatch">Data Patch Panel</li>
@@ -201,31 +215,20 @@ $(function() {
 </div>
 
 
-<div class="beta rackdiv">
+  <div class="beta rackdiv">
     <div data-role="fieldcontain" >
-        <input type="range" name="slider" value="12" min="6" max="48" data-highlight="true" />
+        <input class="racksize" type="range" name="slider" value="12" min="6" max="48" data-highlight="true" >
     </div>
-    <h5>
-      <span class="ui-widget">
-      <select class="racksize">
-        <option value="8">8</option>
-        <option value="12" selected>12</option>
-        <option value="38">38</option>
-        <option value="42" >42</option>
-        <option value="45">45</option>
-      </select>
+    <input type="text" placeholder="rack name" class="rackname"></imput>
+    <span>
+      <button class="save" data-theme="g" id="button">save</button>
+      <button class="trim" data-theme="e" title="Delete all items outside the designated rack space" id="button">trim</button>
+      <button class="clear" data-theme="d"  title="Delete all items and start again with a blank rack" id="button">clear</button>
     </span>
-  RU Rack</h5>
-  <input type="text" placeholder="rack name" class="rackname"></imput>
-  <span>
-  <button class="save" data-theme="g" id="button">save</button>
-  <button class="trim" data-theme="e" title="Delete all items outside the designated rack space" id="button">trim</button>
-  <button class="clear" data-theme="d"  title="Delete all items and start again with a blank rack" id="button">clear</button>
-</span>
-<div></br></div>
-<div height=2px class="btn btn-block btn-lg btn-inverse"></div>
-<h5>Trash</h5>
-<span class="trash"><img src="./image/recyclebin.png"  height="128"></span>
-</div>
+    <h5>Trash</h5>
+    <span class="trash" ><img src="./image/recyclebin.png" height="128" ></span>
+  </div>
 
+
+</div>
 </html>
